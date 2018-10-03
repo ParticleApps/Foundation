4089baadd5e566f93bdf4e90d1957ac2603a2797
Rocco Del Priore <roccodelpriore@me.com>
2018-10-03 00:28:47 -0700

    Initial Commit

---
+++ ParticleFoundation/ParticleFoundation.h
@@ -0,0 +1,19 @@
+//
+//  ParticleFoundation.h
+//  ParticleFoundation
+//
+//  Created by Rocco Del Priore on 10/3/18.
+//  Copyright © 2018 Rocco Del Priore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParticleFoundation.
+FOUNDATION_EXPORT double ParticleFoundationVersionNumber;
+
+//! Project version string for ParticleFoundation.
+FOUNDATION_EXPORT const unsigned char ParticleFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParticleFoundation/PublicHeader.h>
+
+
